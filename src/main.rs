245960849d27c6d067a7b0613295a03b1356db8d
i-m-0-1 <worldnetwork313@gmail.com>
2024-09-25 03:41:16 +0800

    Loop for user multi inputs

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,10 @@
-// TODO: change the user details to another file
 // TODO: make it like terminal, always waiting for commands
 
-use std::env;
-use std::process;
 use std::fs::File;
 use std::path::Path;
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, Write};
 
 fn main() -> io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-
-    if args.len() < 2 {
-        eprintln!("Usage: {} type whoami", args[0]);
-        process::exit(1);
-    }
-
-
-    let input = &args[1];
-   
     let filepath = Path::new("data.txt");
     let file = File::open(&filepath)?;
     let reader = io::BufReader::new(file);
@@ -25,7 +12,7 @@ fn main() -> io::Result<()> {
     let mut user = String::new();
     let mut role = String::new();
 
-    for (index, line ) in reader.lines().enumerate() {
+    for (index, line) in reader.lines().enumerate() {
         let line = line?;
 
         let parts: Vec<&str> = line.split(':').map(|s| s.trim()).collect();
@@ -37,10 +24,35 @@ fn main() -> io::Result<()> {
         }
     }
 
-    match input.as_str() {
-        "whoami" => println!("{}:{}", user, role),
-        _ => println!("Command not found: '{}'", input),
+    loop {
+        print!(":");
+
+        io::stdout().flush()?;
+
+        let mut input = String::new();
+
+        io::stdin().read_line(&mut input)?;
+
+        let command = input.trim();
+
+        match command {
+
+            "whoami" => println!("{}", user),
+
+            "role" => println!("{}", role),
+
+            "exit" => {
+                println!("**Terminal closed**");
+                break;
+            }
+
+            _ => {
+                eprintln!("Command '{}' not found.", command);
+                eprintln!("Usage: 'whoami', 'role', 'exit'");
+            }
+        }
     }
 
-       Ok(())
+    Ok(())
+
 }
